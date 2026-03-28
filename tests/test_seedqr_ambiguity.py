6b0695d05977cd76a50e295cd709df955958259e
earthdiver1 <earthdiver1@gmail.com>
2026-03-29 07:46:42 +0900

    Stabilize ambiguity tests with deterministic ambiguous fixtures

--- tests/test_seedqr_ambiguity.py
+++ tests/test_seedqr_ambiguity.py
@@ -1,7 +1,7 @@
 from base import BaseTest, FlowStep, FlowTest
 
 from seedsigner.helpers import kef
-from seedsigner.models.decode_qr import DecodeQR
+from seedsigner.models.decode_qr import DecodeQR, SeedPayloadAnalysis
 from seedsigner.models.qr_type import QRType
 from seedsigner.models.settings_definition import SettingsConstants
 from seedsigner.views import scan_views
@@ -15,6 +15,8 @@ OUTER_ID = b"outer id"
 OUTER_VERSION = 5
 OUTER_ITERATIONS = 100000
 
+AMBIGUOUS_SEGMENT = bytes([0] * 32)
+
 
 def build_nested_encrypted_qr(inner_payload: bytes) -> bytes:
     cipher = kef.Cipher(OUTER_KEY, OUTER_ID, OUTER_ITERATIONS)
@@ -23,12 +25,22 @@ def build_nested_encrypted_qr(inner_payload: bytes) -> bytes:
 
 
 class TestSeedQRAmbiguity(BaseTest):
-    def test_detect_segment_type_prefers_compactseedqr_by_default(self):
+    def test_detect_segment_type_prefers_compactseedqr_by_default(self, monkeypatch):
+        monkeypatch.setattr(
+            DecodeQR,
+            "_parse_encrypted_qr",
+            staticmethod(lambda segment: (object(), "Encrypted QR Code:\nID: test")),
+        )
         decoder = DecodeQR()
 
-        assert decoder.detect_segment_type(ECB_ENCRYPTED_QR) == QRType.SEED__COMPACTSEEDQR
+        assert decoder.detect_segment_type(AMBIGUOUS_SEGMENT) == QRType.SEED__COMPACTSEEDQR
 
-    def test_detect_segment_type_prompts_for_ambiguous_seed_qr(self):
+    def test_detect_segment_type_prompts_for_ambiguous_seed_qr(self, monkeypatch):
+        monkeypatch.setattr(
+            DecodeQR,
+            "_parse_encrypted_qr",
+            staticmethod(lambda segment: (object(), "Encrypted QR Code:\nID: test")),
+        )
         self.settings.set_value(
             SettingsConstants.SETTING__AMBIGUOUS_SEED_QR,
             SettingsConstants.AMBIGUOUS_SEED_QR__PROMPT,
@@ -36,11 +48,16 @@ class TestSeedQRAmbiguity(BaseTest):
         )
         decoder = DecodeQR()
 
-        assert decoder.detect_segment_type(ECB_ENCRYPTED_QR) == QRType.SEED__AMBIGUOUS
+        assert decoder.detect_segment_type(AMBIGUOUS_SEGMENT) == QRType.SEED__AMBIGUOUS
 
 
 class TestSeedQRAmbiguityFlows(FlowTest):
-    def test_scan_ambiguous_seed_qr_can_be_routed_to_encrypted_flow(self):
+    def test_scan_ambiguous_seed_qr_can_be_routed_to_encrypted_flow(self, monkeypatch):
+        monkeypatch.setattr(
+            DecodeQR,
+            "_parse_encrypted_qr",
+            staticmethod(lambda segment: (object(), "Encrypted QR Code:\nID: test")),
+        )
         self.settings.set_value(
             SettingsConstants.SETTING__AMBIGUOUS_SEED_QR,
             SettingsConstants.AMBIGUOUS_SEED_QR__PROMPT,
@@ -48,7 +65,7 @@ class TestSeedQRAmbiguityFlows(FlowTest):
         )
 
         def load_ambiguous_encrypted_qr(view: scan_views.ScanView):
-            view.decoder.add_data(ECB_ENCRYPTED_QR)
+            view.decoder.add_data(AMBIGUOUS_SEGMENT)
 
         self.run_sequence([
             FlowStep(MainMenuView, button_data_selection=MainMenuView.SCAN),
@@ -60,13 +77,30 @@ class TestSeedQRAmbiguityFlows(FlowTest):
             FlowStep(scan_views.ScanEncryptedQREncryptionKeyView),
         ])
 
-    def test_nested_encrypted_qr_prompts_before_redecrypting(self):
+    def test_nested_encrypted_qr_prompts_before_redecrypting(self, monkeypatch):
         self.settings.set_value(
             SettingsConstants.SETTING__AMBIGUOUS_SEED_QR,
             SettingsConstants.AMBIGUOUS_SEED_QR__PROMPT,
             save=False,
         )
         nested_payload = build_nested_encrypted_qr(ECB_ENCRYPTED_QR)
+        original_analyze_seed_payload = DecodeQR.analyze_seed_payload
+
+        def patched_analyze_seed_payload(segment: bytes):
+            if segment == ECB_ENCRYPTED_QR:
+                return SeedPayloadAnalysis(
+                    segment=segment,
+                    candidate_types=[QRType.SEED__COMPACTSEEDQR, QRType.SEED__ENCRYPTEDQR],
+                    public_data="Encrypted QR Code:\nID: test",
+                    encrypted_qr=object(),
+                )
+            return original_analyze_seed_payload(segment)
+
+        monkeypatch.setattr(
+            DecodeQR,
+            "analyze_seed_payload",
+            staticmethod(patched_analyze_seed_payload),
+        )
 
         self.run_sequence(
             [
